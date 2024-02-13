7ee54f06e1b65c3ee64819d2bdde2a09ea97bb04
Kiprian Berbatov <kiprian.berbatov@manchester.ac.uk>
2024-02-13 20:58:50 +0000

    Fixed wrong scanning logic

--- main/algebra/main_matrix_sparse_laplacian_symmetric.c
+++ main/algebra/main_matrix_sparse_laplacian_symmetric.c
@@ -13,6 +13,7 @@ int main(int argc, char * argv[])
   int * m_cn;
   double ** m_inner;
   matrix_sparse ** m_laplacian;
+  FILE * m_laplacian_file, * m_inner_file;
   
   if (argc != 7)
   {
@@ -29,12 +30,24 @@ int main(int argc, char * argv[])
   
   m_laplacian_name = argv[2];
   m_laplacian_format = argv[3];
-  m_laplacian = matrix_sparse_array_fscan_by_name(m_laplacian_name, d + 1,
-                                                  m_laplacian_format);
+  // m_laplacian = matrix_sparse_array_fscan_by_name(m_laplacian_name, d + 1,
+  //                                                 m_laplacian_format);
+  m_laplacian_file = fopen(m_laplacian_name, "r");
+  if (errno)
+  {
+    fputs("main - cannot open file containing m_laplacian\n", stderr);
+    goto end;
+  }
+  m_laplacian = matrix_sparse_array_fscan(
+    m_laplacian_file,
+    d + 1,
+    m_laplacian_format
+  );
+  // matrix_sparse_array_fprint(stderr, d + 1, m_laplacian, "--raw");
   if (errno)
   {
     fputs("main - cannot scan for m_laplacian\n", stderr);
-    goto end;
+    goto m_laplacian_file_close;
   }
   
   m_cn = (int *) malloc(sizeof(int) * (d + 1));
@@ -48,12 +61,24 @@ int main(int argc, char * argv[])
   
   m_inner_name = argv[4];
   m_inner_format = argv[5];
-  m_inner = 
-    double_array2_fscan_by_name(m_inner_name, d + 1, m_cn, m_inner_format);
+  m_inner_file = fopen(m_inner_name, "r");
+  if (errno)
+  {
+    fputs("main - cannot open file containing m_inner\n", stderr);
+    goto m_cn_free;
+  }
+  m_inner = double_array2_fscan(
+    m_inner_file,
+    d + 1,
+    m_cn,
+    m_inner_format
+  );
+  // m_inner = 
+  //   double_array2_fscan_by_name(m_inner_name, d + 1, m_cn, m_inner_format);
   if (errno)
   {
     fputs("main - cannot  scan m_inner\n", stderr);
-    goto m_cn_free;
+    goto m_inner_file_close;
   }
   
   matrix_sparse_laplacian_symmetric(m_laplacian, d, m_inner);
@@ -62,10 +87,14 @@ int main(int argc, char * argv[])
   matrix_sparse_array_fprint(stdout, d + 1, m_laplacian, out_format);
 
   double_array2_free(m_inner, d + 1);
+m_inner_file_close:
+  fclose(m_inner_file);
 m_cn_free:
   free(m_cn);
 m_laplacian_free:
-  matrix_sparse_array_free(m_laplacian, d + 1);  
+  matrix_sparse_array_free(m_laplacian, d + 1);
+m_laplacian_file_close:
+  fclose(m_laplacian_file);
 end:
   return errno;
 }
