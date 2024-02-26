5a355edfd2c4e95f0cb39b0fd3c63cbb3dbb0a7c
Kiprian Berbatov <kiprian.berbatov@manchester.ac.uk>
2024-02-26 12:57:14 +0000

    Better documentation for matrix_sparse.h

--- include/algebra/matrix_sparse.h
+++ include/algebra/matrix_sparse.h
@@ -13,11 +13,14 @@ typedef struct matrix_sparse
   double * values;
 } matrix_sparse;
 
+/* find a[i][j] */
 double matrix_sparse_part(const matrix_sparse * a, int i, int j);
 
 /***************************** matrix_sparse_free *****************************/
+/* deallocate heap-allocated sparse matrix a */
 void matrix_sparse_free(matrix_sparse * a);
 
+/* deallocate heap-allocated sparse matrix a without topology information */
 void matrix_sparse_free_shared(matrix_sparse * a);
 
 /**************************** matrix_sparse_fscan *****************************/
@@ -25,10 +28,13 @@ void matrix_sparse_free_shared(matrix_sparse * a);
  * --raw
  */
 
+/* read a matrix from file "in" given in format "format" */
 matrix_sparse * matrix_sparse_fscan(FILE * in, const char * format);
 
+/* open a file, read the matrix in the specified format and close the file */
 matrix_sparse * matrix_sparse_fscan_by_name(
-  const char * name, const char * format);
+  const char * name,
+  const char * format);
 
 
 /**************************** matrix_sparse_fprint ****************************/
@@ -41,102 +47,192 @@ matrix_sparse * matrix_sparse_fscan_by_name(
  * --mathematica-sparse
  */
 
+/* print in file "out" matrix "a" in format "format" */
 void matrix_sparse_fprint(
-  FILE * out, const matrix_sparse * a, const char * format);
-
-/********************* matrix_sparse algebraic operations *********************/
-matrix_sparse * matrix_sparse_identity(int n);
-
-matrix_sparse * matrix_sparse_linear_combination(
-  const matrix_sparse * a, const matrix_sparse * b, double alpha, double beta);
-
-matrix_sparse * matrix_sparse_add(
-  const matrix_sparse * a, const matrix_sparse * b);
-
-matrix_sparse * matrix_sparse_product(
-  const matrix_sparse * a, const matrix_sparse * b);
-
-matrix_sparse * matrix_sparse_transpose(const matrix_sparse * a);
-
-matrix_sparse * matrix_sparse_laplacian_0(const matrix_sparse * a_0, const matrix_sparse * b_1);
-
-matrix_sparse * matrix_sparse_laplacian_p(
-  const matrix_sparse * a_p_minus_1, const matrix_sparse * a_p, 
-  const matrix_sparse * b_p, const matrix_sparse * b_p_plus_1);
-
-matrix_sparse * matrix_sparse_laplacian_d(
-  const matrix_sparse * a_d_minus_1, const matrix_sparse * b_d);
-
-matrix_sparse ** matrix_sparse_laplacian(
-  int d, matrix_sparse ** a, matrix_sparse ** b);
-
-void matrix_sparse_laplacian_fprint(
-  FILE * out, int d, matrix_sparse ** a, matrix_sparse ** b,
+  FILE * out,
+  const matrix_sparse * a,
   const char * format);
 
-void matrix_sparse_laplacian_symmetric(
-  matrix_sparse ** delta, int d, double ** m_inner);
+/********************* matrix_sparse algebraic operations *********************/
+/* creates a sparse matrix representing the identity matrix */
+matrix_sparse * matrix_sparse_identity(int n);
 
-double * matrix_sparse_diffusion(
-  const matrix_sparse * laplacian_0, const double * lhs, const double * u_0,
-  double tau, int N);
+/* return c = alpha * a + beta * b */
+matrix_sparse * matrix_sparse_linear_combination(
+  const matrix_sparse * a,
+  const matrix_sparse * b,
+  double alpha,
+  double beta);
 
-typedef double (*scalar_field)(const double *);
+/* return c = a + b */
+matrix_sparse * matrix_sparse_add(
+  const matrix_sparse * a,
+  const matrix_sparse * b);
 
-typedef double (*time_dependent_scalar_field)(double, const double *);
+/* return c = a . b */
+matrix_sparse * matrix_sparse_product(
+  const matrix_sparse * a,
+  const matrix_sparse * b);
 
-double * matrix_sparse_laplace_equation_solve(
-  const matrix_sparse * m_laplacian, int m_dim_embedded, const double * m_coord,
-  const jagged1 * m_nodes_bd, scalar_field f, scalar_field g_d);
+/* return b = a^T */
+matrix_sparse * matrix_sparse_transpose(const matrix_sparse * a);
 
-double * matrix_sparse_laplace_equation_solve_non_grid(
-  const matrix_sparse * m_laplacian, int m_dim_embedded, const double * m_coord,
-  const double * m_inner, const jagged1 * m_nodes_bd, scalar_field f,
-  scalar_field g_d);
-
-double * matrix_sparse_heat_conduction_solve_non_grid(
-  const matrix_sparse * m_laplacian, int m_dim_embedded, const double * m_coord,
-  const double * m_inner, const jagged1 * m_nodes_bd,
-  time_dependent_scalar_field f, time_dependent_scalar_field g_d,
-  scalar_field u_0, double tau, int N);
+/* y := a . x + b */
+void matrix_sparse_vector_multiply_add(
+  double * y,
+  const matrix_sparse * a,
+  const double * x);
 
 /************************* matrix_sparse_linear_solve *************************/
 /* Supported methods for "matrix_sparse_linear_solve":
  * --cholesky
  */
 
+/* solve a x = b with a given "method", where x overwrites b;
+however, temporary memory is still required */
 void matrix_sparse_linear_solve(
-  const matrix_sparse * a, double * b, const char * method);
+  const matrix_sparse * a,
+  double * b,
+  const char * method);
 
 /**************************** matrix_sparse_remove ****************************/
+
+/* restrict matrix a to specified rows and columns */
 matrix_sparse * matrix_sparse_restrict(
-  const matrix_sparse * a, const jagged1 * rows_complement,
+  const matrix_sparse * a,
+  const jagged1 * rows_complement,
   const jagged1 * cols_complement);
 
+/* restrict the symmetric matrix a to the same rows and columns */
 matrix_sparse * matrix_sparse_restrict_symmetric(
-  const matrix_sparse * a, const jagged1 * rows_complement);
+  const matrix_sparse * a,
+  const jagged1 * rows_complement);
 
+/* remove specified rows and columns from matrix a */
 matrix_sparse * matrix_sparse_remove(
-  const matrix_sparse * a, const jagged1 * rows, const jagged1 * cols);
+  const matrix_sparse * a,
+  const jagged1 * rows,
+  const jagged1 * cols);
 
+/* remove the same rows and columns from symmetric matrix a */
 matrix_sparse * matrix_sparse_remove_symmetric(
-  const matrix_sparse * a, const jagged1 * rows);
+  const matrix_sparse * a,
+  const jagged1 * rows);
 
+/* solve a_0 x = b, where a_0 = matrix_sparse_remove_symmetric(a, rows) */
 double * matrix_sparse_constrained_solve_cholesky(
-  const matrix_sparse * a, const double * b, const jagged1 * rows);
+  const matrix_sparse * a,
+  const double * b,
+  const jagged1 * rows);
+
+/************** matrix_sparse: preocedures for physical problems **************/
+/* return b_1 . a_0 */
+matrix_sparse * matrix_sparse_laplacian_0(
+  const matrix_sparse * a_0,
+  const matrix_sparse * b_1);
+
+/* return a_p_minus_1 . b_p + a_p . b_p_plus_1 */
+matrix_sparse * matrix_sparse_laplacian_p(
+  const matrix_sparse * a_p_minus_1,
+  const matrix_sparse * a_p, 
+  const matrix_sparse * b_p,
+  const matrix_sparse * b_p_plus_1);
+
+/* a_d_minus_1 . b_d */
+matrix_sparse * matrix_sparse_laplacian_d(
+  const matrix_sparse * a_d_minus_1,
+  const matrix_sparse * b_d);
+
+/* return the array if all laplacians */
+matrix_sparse ** matrix_sparse_laplacian(
+  int d,
+  matrix_sparse ** a,
+  matrix_sparse ** b);
+
+/* calculate laplacians and print them with freeing result on each step */
+void matrix_sparse_laplacian_fprint(
+  FILE * out,
+  int d,
+  matrix_sparse ** a,
+  matrix_sparse ** b,
+  const char * format);
+
+/* find the symmetric version of laplacian that was computed in orthogonal but
+not normalized basis */
+void matrix_sparse_laplacian_symmetric(
+  matrix_sparse ** delta,
+  int d,
+  double ** m_inner);
+
+/* supposedly solve the diffusion equation -- for now this is broken */
+double * matrix_sparse_diffusion(
+  const matrix_sparse * laplacian_0,
+  const double * lhs,
+  const double * u_0,
+  double tau,
+  int N);
+
+typedef double (*scalar_field)(const double *);
+
+typedef double (*time_dependent_scalar_field)(double, const double *);
+
+/* solve the laplace equation on a grid */
+double * matrix_sparse_laplace_equation_solve(
+  const matrix_sparse * m_laplacian,
+  int m_dim_embedded,
+  const double * m_coord,
+  const jagged1 * m_nodes_bd,
+  scalar_field f,
+  scalar_field g_d);
+
+/* solve the Laplace equation on a non-grid by using symmetrization and then
+the Cholesky method */
+double * matrix_sparse_laplace_equation_solve_non_grid(
+  const matrix_sparse * m_laplacian,
+  int m_dim_embedded,
+  const double * m_coord,
+  const double * m_inner,
+  const jagged1 * m_nodes_bd,
+  scalar_field f,
+  scalar_field g_d);
+
+/* supposedly solving the heat equation with Dirichlet boundary conditions */
+double * matrix_sparse_heat_conduction_solve_non_grid(
+  const matrix_sparse * m_laplacian,
+  int m_dim_embedded,
+  const double * m_coord,
+  const double * m_inner,
+  const jagged1 * m_nodes_bd,
+  time_dependent_scalar_field f,
+  time_dependent_scalar_field g_d,
+  scalar_field u_0,
+  double tau,
+  int N);
 
 /***************************** matrix_sparse_array ****************************/
+/* scan an array of matrices of size n */
 matrix_sparse ** matrix_sparse_array_fscan(
-  FILE * in, int n, const char * format);
+  FILE * in,
+  int n,
+  const char * format);
 
+/* broken, do not use, better to open a file and the scan */
 matrix_sparse ** matrix_sparse_array_fscan_by_name(
-  const char * name, int n, const char * format);
+  const char * name,
+  int n,
+  const char * format);
 
+/* print an array of matrices */
 void matrix_sparse_array_fprint(
-  FILE * out, int n, matrix_sparse ** a, const char * format);
+  FILE * out,
+  int n,
+  matrix_sparse ** a,
+  const char * format);
 
+/* deallocate an array of n matrices */
 void matrix_sparse_array_free(matrix_sparse ** a, int n);
 
+/* deallocate an array of n matrices sharing a common stencil */
 void matrix_sparse_array_free_shared(matrix_sparse ** a, int n);
 
 #endif /* MATRIX_SPARSE_H */
