4eaaf34ad3e2bbe76d3c386d6e96523a5696c9af
Kiprian Berbatov <kiprian.berbatov@manchester.ac.uk>
2024-06-03 13:46:37 +0000

    Fix jagged3 and jagged4 printing bugs

--- src/array/jagged3_file_print_raw.c
+++ src/array/jagged3_file_print_raw.c
@@ -10,6 +10,6 @@ void jagged3_file_print_raw(FILE * out, const jagged3 * arr)
   int_array_file_print(out, arr->a0, arr->a1, "--raw");
   arr_a2_size = int_array_total_sum(arr->a0, arr->a1);
   int_array_file_print(out, arr_a2_size, arr->a2, "--raw");
-  arr_a3_size = int_array_total_sum(arr_a2_size, arr->a3);
+  arr_a3_size = int_array_total_sum(arr_a2_size, arr->a2);
   int_array_file_print(out, arr_a3_size, arr->a3, "--raw");
 }
--- src/array/jagged4_file_print_raw.c
+++ src/array/jagged4_file_print_raw.c
@@ -9,7 +9,7 @@ void jagged4_file_print_raw(FILE * out, const jagged4 * arr)
   int_array_file_print(out, arr->a0, arr->a1, "--raw");
   arr_a2_size = int_array_total_sum(arr->a0, arr->a1);
   int_array_file_print(out, arr_a2_size, arr->a2, "--raw");
-  arr_a3_size = int_array_total_sum(arr_a2_size, arr->a3);
+  arr_a3_size = int_array_total_sum(arr_a2_size, arr->a2);
   int_array_file_print(out, arr_a3_size, arr->a3, "--raw");
   arr_a4_size = int_array_total_sum(arr_a3_size, arr->a3);
   int_array_file_print(out, arr_a4_size, arr->a4, "--raw");
