4e8c4317c15b4d41df3c21087eece77ac07291cd
Kiprian Berbatov <kiprian.berbatov@manchester.ac.uk>
2024-07-03 08:03:04 +0000

    Add elasticity example

--- make/docs/latex.mk
+++ make/docs/latex.mk
@@ -5,8 +5,12 @@ docs:
 	mkdir -p $@
 
 docs/main.pdf: latex/main.tex | docs
-	pdflatex -output-directory=docs $<
-	pdflatex -output-directory=docs $<
+	pdflatex -output-directory=$| $<
+	pdflatex -output-directory=$| $<
+
+.PHONY: docs_all_fast
+docs_all_fast: latex/main.tex | docs
+	pdflatex -output-directory=$| $<
 
 .PHONY: docs_clean
 docs_clean:
--- make/site.mk
+++ make/site.mk
@@ -2,16 +2,20 @@
 site: website/index.html website/style.css website/main.pdf
 
 website/index.html: html/index.html | website
-	cp $< website
+	cp $< $|
 
 website/style.css: css/style.css | website
-	cp $< website
+	cp $< $|
 
 website/main.pdf: docs/main.pdf | website
-	cp $< website
+	cp $< $|
 
 website:
 	mkdir -p $@
+
+.PHONY: site_fast
+site_fast: website/index.html website/style.css docs_all_fast | website
+	cp docs/main.pdf $|
 
 .PHONY: site_clean
 site_clean:
