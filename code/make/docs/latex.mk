e1a3a79eebb57483a9336660db12c0fdad263434
Kiprian Berbatov <kiprian.berbatov@manchester.ac.uk>
2024-08-23 17:20:04 +0000

    Documentation: fix typos and explain physical dimensions

--- code/make/docs/latex.mk
+++ code/make/docs/latex.mk
@@ -5,8 +5,8 @@ build/docs: | build
 	mkdir -p $@
 
 build/docs/main.pdf: code/latex/main.tex | build/docs
-	pdflatex -output-directory=$| $<
-	pdflatex -output-directory=$| $<
+	pdflatex -halt-on-error -output-directory=$| $<
+	pdflatex -halt-on-error -output-directory=$| $<
 
 .PHONY: docs_fast
 docs_fast: code/latex/main.tex | build/docs
