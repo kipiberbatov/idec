a3f90e7f8cddee687e53f053c7a8de904e9dee66
Kiprian Berbatov <kiprian.berbatov@manchester.ac.uk>
2024-08-28 15:16:01 +0000

    Divide monolithic LaTeX document into its constituents

--- code/make/docs/latex.mk
+++ code/make/docs/latex.mk
@@ -4,13 +4,15 @@ docs: build/docs/main.pdf
 build/docs: | build
 	mkdir -p $@
 
+LATEX_COMPILER := TEXINPUTS=./code/latex/src: pdflatex -halt-on-error
+
 build/docs/main.pdf: code/latex/main.tex | build/docs
-	pdflatex -halt-on-error -output-directory=$| $<
-	pdflatex -halt-on-error -output-directory=$| $<
+	$(LATEX_COMPILER) -output-directory=$| $<
+	$(LATEX_COMPILER) -output-directory=$| $<
 
 .PHONY: docs_fast
 docs_fast: code/latex/main.tex | build/docs
-	pdflatex -halt-on-error -output-directory=$| $<
+	$(LATEX_COMPILER) -output-directory=$| $<
 
 .PHONY: docs_clean
 docs_clean:
