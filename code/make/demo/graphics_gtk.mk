3032d657f162abea70d5a87cb913b7270c613be4
Kiprian Berbatov <kiprian.berbatov@manchester.ac.uk>
2024-09-19 09:52:45 +0000

    Separate all diffusion demos in their directory

--- code/make/demo/graphics_gtk.mk
+++ code/make/demo/graphics_gtk.mk
@@ -25,7 +25,7 @@ build/$(MODE)/demo/graphics/log/fill_gtk.log:\
 build/$(MODE)/demo/graphics/log/mesh_brick_2d_2_forman_diffusion_transient_continuous_p2_temperature_gtk.log:\
   build/$(MODE)/bin/gtk_mesh_2d_colored_zero_cochain_sequence$(.EXE)\
   build/$(MODE)/demo/mesh/mesh_brick_2d_2_forman.txt\
-  build/$(MODE)/demo/mesh/mesh_brick_2d_2_forman_diffusion_transient_continuous_p2_temperature.txt\
+  build/$(MODE)/demo/diffusion/mesh_brick_2d_2_forman_diffusion_transient_continuous_p2_temperature.txt\
   | build/$(MODE)/demo/graphics/log
 	$< --raw $(word 2, $^) 100 --raw $(word 3, $^) > $@
 
@@ -33,56 +33,56 @@ build/$(MODE)/demo/graphics/log/mesh_brick_2d_2_forman_diffusion_transient_conti
 build/$(MODE)/demo/graphics/log/mesh_brick_2d_5_forman_diffusion_transient_continuous_p2_temperature_gtk.log:\
   build/$(MODE)/bin/gtk_mesh_2d_colored_zero_cochain_sequence$(.EXE)\
   build/$(MODE)/demo/mesh/mesh_brick_2d_5_forman.txt\
-  build/$(MODE)/demo/mesh/mesh_brick_2d_5_forman_diffusion_transient_continuous_p2_temperature.txt\
+  build/$(MODE)/demo/diffusion/mesh_brick_2d_5_forman_diffusion_transient_continuous_p2_temperature.txt\
   | build/$(MODE)/demo/graphics/log
 	$< --raw $(word 2, $^) 1000 --raw $(word 3, $^) > $@
 
 build/$(MODE)/demo/graphics/log/mesh_brick_2d_5_forman_diffusion_transient_continuous_p4_temperature_gtk.log:\
   build/$(MODE)/bin/gtk_mesh_2d_colored_zero_cochain_sequence$(.EXE)\
   build/$(MODE)/demo/mesh/mesh_brick_2d_5_forman.txt\
-  build/$(MODE)/demo/mesh/mesh_brick_2d_5_forman_diffusion_transient_continuous_p4_temperature.txt\
+  build/$(MODE)/demo/diffusion/mesh_brick_2d_5_forman_diffusion_transient_continuous_p4_temperature.txt\
   | build/$(MODE)/demo/graphics/log
 	$< --raw $(word 2, $^) 10000 --raw $(word 3, $^) > $@
 
 build/$(MODE)/demo/graphics/log/mesh_brick_2d_2_forman_diffusion_transient_continuous_p4_temperature_gtk.log:\
   build/$(MODE)/bin/gtk_diffusion$(.EXE)\
   build/$(MODE)/demo/mesh/mesh_brick_2d_2_forman.txt\
-  build/$(MODE)/demo/mesh/mesh_brick_2d_2_forman_diffusion_transient_continuous_p4_temperature.txt\
+  build/$(MODE)/demo/diffusion/mesh_brick_2d_2_forman_diffusion_transient_continuous_p4_temperature.txt\
   | build/$(MODE)/demo/graphics/log
 	$< --raw $(word 2, $^) $(word 3, $^) > $@
 
 build/$(MODE)/demo/graphics/log/mesh_brick_2d_10_forman_diffusion_transient_continuous_p4_temperature_gtk.log:\
   build/$(MODE)/bin/gtk_mesh_2d_colored_zero_cochain_sequence$(.EXE)\
   build/$(MODE)/demo/mesh/mesh_brick_2d_10_forman.txt\
-  build/$(MODE)/demo/mesh/mesh_brick_2d_10_forman_diffusion_transient_continuous_p4_temperature.txt\
+  build/$(MODE)/demo/diffusion/mesh_brick_2d_10_forman_diffusion_transient_continuous_p4_temperature.txt\
   | build/$(MODE)/demo/graphics/log
 	$< --raw $(word 2, $^) 4000 --raw $(word 3, $^) > $@
 
 build/$(MODE)/demo/graphics/log/mesh_brick_2d_10_forman_diffusion_transient_continuous_p4_flux_gtk.log:\
   build/$(MODE)/bin/gtk_mesh_2d_colored_one_cochain_sequence$(.EXE)\
   build/$(MODE)/demo/mesh/mesh_brick_2d_10_forman.txt\
-  build/$(MODE)/demo/mesh/mesh_brick_2d_10_forman_diffusion_transient_continuous_p4_flux.txt\
+  build/$(MODE)/demo/diffusion/mesh_brick_2d_10_forman_diffusion_transient_continuous_p4_flux.txt\
   | build/$(MODE)/demo/graphics/log
 	$< --raw $(word 2, $^) 4000 --raw $(word 3, $^) > $@
 
 build/$(MODE)/demo/graphics/log/mesh_2d_10_grains_forman_diffusion_transient_continuous_p7_temperature_gtk.log:\
   build/$(MODE)/bin/gtk_mesh_2d_colored_zero_cochain_sequence$(.EXE)\
   build/$(MODE)/demo/mesh/mesh_2d_10_grains_forman.txt\
-  build/$(MODE)/demo/mesh/mesh_2d_10_grains_forman_diffusion_transient_continuous_p7_temperature.txt\
+  build/$(MODE)/demo/diffusion/mesh_2d_10_grains_forman_diffusion_transient_continuous_p7_temperature.txt\
   | build/$(MODE)/demo/graphics/log
 	$< --raw $(word 2, $^) 1000 --raw $(word 3, $^) > $@
 
 build/$(MODE)/demo/graphics/log/mesh_2d_10_grains_forman_diffusion_transient_continuous_p7_flux_gtk.log:\
   build/$(MODE)/bin/gtk_mesh_2d_colored_one_cochain_sequence$(.EXE)\
   build/$(MODE)/demo/mesh/mesh_2d_10_grains_forman.txt\
-  build/$(MODE)/demo/mesh/mesh_2d_10_grains_forman_diffusion_transient_continuous_p7_flux.txt\
+  build/$(MODE)/demo/diffusion/mesh_2d_10_grains_forman_diffusion_transient_continuous_p7_flux.txt\
   | build/$(MODE)/demo/graphics/log
 	$< --raw $(word 2, $^) 1000 --raw $(word 3, $^) > $@
 
 build/$(MODE)/demo/graphics/log/mesh_square_8_diffusion_transient_continuous_p5_temperature_gtk.log:\
   build/$(MODE)/bin/gtk_mesh_2d_colored_zero_cochain_sequence$(.EXE)\
   build/$(MODE)/demo/mesh/mesh_square_8.txt\
-  build/$(MODE)/demo/mesh/mesh_square_8_diffusion_transient_continuous_p5_temperature.txt\
+  build/$(MODE)/demo/diffusion/mesh_square_8_diffusion_transient_continuous_p5_temperature.txt\
   | build/$(MODE)/demo/graphics/log
 	$< --raw $(word 2, $^) 4000 --raw $(word 3, $^) > $@
 
