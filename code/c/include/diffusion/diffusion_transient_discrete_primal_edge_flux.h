8bd7e34febcd66c9ae4bbda40553869ea6c54ebd
Kiprian Berbatov <kiprian.berbatov@manchester.ac.uk>
2024-10-28 13:21:57 +0000

    Add const qualifiers

--- code/c/include/diffusion/diffusion_transient_discrete_primal_edge_flux.h
+++ code/c/include/diffusion/diffusion_transient_discrete_primal_edge_flux.h
@@ -6,7 +6,7 @@
 void diffusion_transient_discrete_primal_edge_flux(
   double * flux,
   const mesh * m,
-  matrix_sparse * m_bd_1,
+  const matrix_sparse * m_bd_1,
   const double * pi_1,
   int number_of_steps,
   const double * temperature);
