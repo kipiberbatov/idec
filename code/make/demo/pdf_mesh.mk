a21a1abd97b563c662b4f7642189b821366d4c68
Kiprian Berbatov <kiprian.berbatov@manchester.ac.uk>
2024-10-29 17:10:25 +0000

    Draw regular grids in PDF files

--- code/make/demo/pdf_mesh.mk
+++ code/make/demo/pdf_mesh.mk
@@ -2,6 +2,8 @@ build/$(MODE)/demo/pdf/mesh: | build/$(MODE)/demo/pdf
 	mkdir -p $@
 
 _demo_pdf_mesh :=\
+  build/$(MODE)/demo/pdf/mesh/brick_2d_10_edge_skeleton.pdf\
+  build/$(MODE)/demo/pdf/mesh/brick_2d_10_forman_edge_skeleton.pdf\
   build/$(MODE)/demo/pdf/mesh/circular_4_3_edge_skeleton.pdf\
   build/$(MODE)/demo/pdf/mesh/circular_4_3_forman_edge_skeleton.pdf\
   build/$(MODE)/demo/pdf/mesh/circular_17_10_edge_skeleton.pdf\
@@ -12,6 +14,18 @@ _demo_pdf_mesh :=\
   build/$(MODE)/demo/pdf/mesh/brick_2d_10_forman_edge_skeleton.pdf\
   build/$(MODE)/demo/pdf/mesh/2d_10_grains_edge_skeleton.pdf\
   build/$(MODE)/demo/pdf/mesh/2d_10_grains_forman_edge_skeleton.pdf\
+
+build/$(MODE)/demo/pdf/mesh/brick_2d_10_edge_skeleton.pdf:\
+  build/$(MODE)/bin/pdf_mesh_2d_colored_edge_skeleton$(.EXE)\
+  build/$(MODE)/demo/mesh/brick_2d_10.txt\
+  | build/$(MODE)/demo/pdf/mesh
+	$< --raw $(word 2, $^) $@
+
+build/$(MODE)/demo/pdf/mesh/brick_2d_10_forman_edge_skeleton.pdf:\
+  build/$(MODE)/bin/pdf_mesh_2d_colored_edge_skeleton$(.EXE)\
+  build/$(MODE)/demo/mesh/brick_2d_10_forman.txt\
+  | build/$(MODE)/demo/pdf/mesh
+	$< --raw $(word 2, $^) $@
 
 build/$(MODE)/demo/pdf/mesh/circular_4_3_edge_skeleton.pdf:\
   build/$(MODE)/bin/pdf_mesh_2d_colored_edge_skeleton$(.EXE)\
