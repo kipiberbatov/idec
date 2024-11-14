84e4a7bb1cf54b92dd2eb78ebfceaeca639ae44c
Kiprian Berbatov <kiprian.berbatov@manchester.ac.uk>
2024-11-14 14:32:53 +0000

    Add generated by C (with Cairo) meshes to the LaTeX document

--- code/make/docs/latex.mk
+++ code/make/docs/latex.mk
@@ -7,13 +7,13 @@ build/docs: | build
 LATEX_COMPILER := TEXINPUTS=./code/latex/src: pdflatex -halt-on-error
 
 build/docs/main.pdf: code/latex/main.tex $(shell find code/latex -name "*.tex")\
-  | build/docs
+  demo_pdf_mesh | build/docs
 	$(LATEX_COMPILER) -output-directory=$| $<
 	$(LATEX_COMPILER) -output-directory=$| $<
 
 .PHONY: docs_fast
 docs_fast: code/latex/main.tex $(shell find code/latex -name "*.tex")\
-  | build/docs
+  demo_pdf_mesh | build/docs
 	$(LATEX_COMPILER) -output-directory=$| $<
 
 .PHONY: docs_clean
