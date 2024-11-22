96bf0b52b7fc21239f42b45a3b4332fecd93720a
Kiprian Berbatov <kiprian.berbatov@manchester.ac.uk>
2024-11-22 11:12:08 +0000

    Introduce empty file targets for fast LaTeX builds

--- code/make/docs/latex.mk
+++ code/make/docs/latex.mk
@@ -1,21 +1,25 @@
-.PHONY: docs
-docs: build/docs/main.pdf
+.PHONY: docs docs_fast docs_clean
 
 build/docs: | build
 	mkdir -p $@
 
 LATEX_COMPILER := TEXINPUTS=./code/latex/src: pdflatex -halt-on-error
 
+docs: build/docs/main.pdf
+
 build/docs/main.pdf: code/latex/main.tex $(shell find code/latex -name "*.tex")\
-  demo_pdf | build/docs
+  $(_demo_pdf) | build/docs
 	$(LATEX_COMPILER) -output-directory=$| $<
 	$(LATEX_COMPILER) -output-directory=$| $<
+	touch build/.docs_fast
 
-.PHONY: docs_fast
-docs_fast: code/latex/main.tex $(shell find code/latex -name "*.tex")\
-  demo_pdf_mesh | build/docs
-	$(LATEX_COMPILER) -output-directory=$| $<
+docs_fast: build/.docs_fast
 
-.PHONY: docs_clean
+build/.docs_fast: code/latex/main.tex $(shell find code/latex -name "*.tex")\
+  $(_demo_pdf_mesh) | build/docs
+	$(LATEX_COMPILER) -output-directory=$| $<
+	touch $@
+
 docs_clean:
 	-$(RM) -r build/docs
+	-$(RM) build/.docs_fast
--- code/make/website.mk
+++ code/make/website.mk
@@ -1,4 +1,8 @@
-.PHONY: website
+.PHONY: website website_fast website_clean
+
+build/website: | build
+	mkdir -p $@
+
 website: build/website/index.html build/website/style.css build/website/main.pdf
 
 build/website/index.html: code/html/index.html | build/website
@@ -9,15 +13,16 @@ build/website/style.css: code/css/style.css | build/website
 
 build/website/main.pdf: build/docs/main.pdf | build/website
 	cp $< $|
+	touch build/.website_fast
 
-build/website: | build
-	mkdir -p $@
+website_fast: build/.website_fast
 
-.PHONY: website_fast
-website_fast: docs_fast build/website/index.html build/website/style.css\
-  | build/docs build/website
+build/.website_fast: build/website/index.html build/website/style.css\
+  | build/docs build/website build/.docs_fast
 	cp $(word 1, $|)/main.pdf $(word 2, $|)
+	touch $@
 
 .PHONY: website_clean
 website_clean:
 	-$(RM) -r build/website
+	-$(RM) build/.website_fast
