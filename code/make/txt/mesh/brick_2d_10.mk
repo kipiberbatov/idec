ab9625a8a62ae5163d85020510f35b7f0253ebd4
Kiprian Berbatov <kiprian.berbatov@manchester.ac.uk>
2025-02-11 22:12:15 +0000

    Add corrected by node curvature operators for brick_2d_10_forman

--- code/make/txt/mesh/brick_2d_10.mk
+++ code/make/txt/mesh/brick_2d_10.mk
@@ -4,7 +4,6 @@ _txt_mesh_brick_2d_10 := \
   build/$(MODE)/txt/mesh/brick_2d_10.txt\
   build/$(MODE)/txt/mesh/brick_2d_10_vol.txt\
   build/$(MODE)/txt/mesh/brick_2d_10_forman.txt\
-  build/$(MODE)/txt/mesh/brick_2d_10_forman_boundary.txt\
   build/$(MODE)/txt/mesh/brick_2d_10_forman_cbd.txt\
   build/$(MODE)/txt/mesh/brick_2d_10_forman_vol.txt\
   build/$(MODE)/txt/mesh/brick_2d_10_forman_metric.txt\
@@ -13,6 +12,12 @@ _txt_mesh_brick_2d_10 := \
   build/$(MODE)/txt/mesh/brick_2d_10_forman_laplacian.txt\
   build/$(MODE)/txt/mesh/brick_2d_10_forman_hodge_coeff.txt\
   build/$(MODE)/txt/mesh/brick_2d_10_forman_hodge.txt\
+  build/$(MODE)/txt/mesh/brick_2d_10_forman_node_curvature.txt\
+  build/$(MODE)/txt/mesh/brick_2d_10_forman_metric_corrected.txt\
+  build/$(MODE)/txt/mesh/brick_2d_10_forman_inner_corrected.txt\
+  build/$(MODE)/txt/mesh/brick_2d_10_forman_cbd_star_corrected.txt\
+  build/$(MODE)/txt/mesh/brick_2d_10_forman_laplacian_corrected.txt\
+  build/$(MODE)/txt/mesh/brick_2d_10_forman_hodge_corrected.txt\
 
 build/$(MODE)/txt/mesh/brick_2d_10.txt:\
   build/$(MODE)/bin/mesh_brick_regular$(.EXE)\
@@ -30,12 +35,6 @@ build/$(MODE)/txt/mesh/brick_2d_10_forman.txt:\
   build/$(MODE)/txt/mesh/brick_2d_10.txt\
   | build/$(MODE)/txt/mesh
 	$< --raw $(word 2, $^) --standard --raw > $@
-
-build/$(MODE)/txt/mesh/brick_2d_10_forman_boundary.txt:\
-  build/$(MODE)/bin/mesh_boundary$(.EXE)\
-  build/$(MODE)/txt/mesh/brick_2d_10_forman.txt\
-  | build/$(MODE)/txt/mesh
-	$< --raw < $(word 2, $^) > $@
 
 build/$(MODE)/txt/mesh/brick_2d_10_forman_cbd.txt:\
   build/$(MODE)/bin/mesh_coboundary$(.EXE)\
@@ -87,6 +86,50 @@ build/$(MODE)/txt/mesh/brick_2d_10_forman_hodge.txt:\
   build/$(MODE)/bin/mesh_qc_hodge$(.EXE)\
   build/$(MODE)/txt/mesh/brick_2d_10_forman.txt\
   build/$(MODE)/txt/mesh/brick_2d_10_forman_inner.txt\
+  build/$(MODE)/txt/mesh/brick_2d_10_forman_hodge_coeff.txt\
+  | build/$(MODE)/txt/mesh
+	$^ > $@
+
+build/$(MODE)/txt/mesh/brick_2d_10_forman_node_curvature.txt:\
+  build/$(MODE)/bin/mesh_node_curvature$(.EXE)\
+  build/$(MODE)/txt/mesh/brick_2d_10_forman.txt\
+  | build/$(MODE)/txt/mesh
+	$< < $(word 2, $^) > $@
+
+build/$(MODE)/txt/mesh/brick_2d_10_forman_metric_corrected.txt:\
+  build/$(MODE)/bin/mesh_qc_metric_corrected$(.EXE)\
+  build/$(MODE)/txt/mesh/brick_2d_10_forman.txt\
+  build/$(MODE)/txt/mesh/brick_2d_10_forman_vol.txt\
+  build/$(MODE)/txt/mesh/brick_2d_10_forman_node_curvature.txt\
+  | build/$(MODE)/txt/mesh
+	$^ > $@
+
+build/$(MODE)/txt/mesh/brick_2d_10_forman_inner_corrected.txt:\
+  build/$(MODE)/bin/mesh_qc_inner$(.EXE)\
+  build/$(MODE)/txt/mesh/brick_2d_10_forman.txt\
+  build/$(MODE)/txt/mesh/brick_2d_10_forman_vol.txt\
+  build/$(MODE)/txt/mesh/brick_2d_10_forman_metric_corrected.txt\
+  | build/$(MODE)/txt/mesh
+	$^ > $@
+
+build/$(MODE)/txt/mesh/brick_2d_10_forman_cbd_star_corrected.txt:\
+  build/$(MODE)/bin/mesh_qc_coboundary_star$(.EXE)\
+  build/$(MODE)/txt/mesh/brick_2d_10_forman.txt\
+  build/$(MODE)/txt/mesh/brick_2d_10_forman_inner_corrected.txt\
+  | build/$(MODE)/txt/mesh
+	$^ > $@
+
+build/$(MODE)/txt/mesh/brick_2d_10_forman_laplacian_corrected.txt:\
+  build/$(MODE)/bin/mesh_qc_laplacian$(.EXE)\
+  build/$(MODE)/txt/mesh/brick_2d_10_forman.txt\
+  build/$(MODE)/txt/mesh/brick_2d_10_forman_cbd_star_corrected.txt\
+  | build/$(MODE)/txt/mesh
+	$^ > $@
+
+build/$(MODE)/txt/mesh/brick_2d_10_forman_hodge_corrected.txt:\
+  build/$(MODE)/bin/mesh_qc_hodge$(.EXE)\
+  build/$(MODE)/txt/mesh/brick_2d_10_forman.txt\
+  build/$(MODE)/txt/mesh/brick_2d_10_forman_inner_corrected.txt\
   build/$(MODE)/txt/mesh/brick_2d_10_forman_hodge_coeff.txt\
   | build/$(MODE)/txt/mesh
 	$^ > $@
