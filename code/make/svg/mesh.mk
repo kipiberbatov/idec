03ff7c141bc1c76208bb426abea76300f7df3172
Kiprian Berbatov <kiprian.berbatov@manchester.ac.uk>
2025-02-20 14:52:49 +0000

    Produce SVGs for all available meshes

--- code/make/svg/mesh.mk
+++ code/make/svg/mesh.mk
@@ -4,16 +4,143 @@ build/$(MODE)/svg/mesh: | build/$(MODE)/svg
 	mkdir -p $@
 
 _svg_mesh :=\
+  build/$(MODE)/svg/mesh/brick_2d_1_edge_skeleton.svg\
+  build/$(MODE)/svg/mesh/brick_2d_1_forman_edge_skeleton.svg\
+  build/$(MODE)/svg/mesh/brick_2d_2_edge_skeleton.svg\
+  build/$(MODE)/svg/mesh/brick_2d_2_forman_edge_skeleton.svg\
+  build/$(MODE)/svg/mesh/brick_2d_4_edge_skeleton.svg\
+  build/$(MODE)/svg/mesh/brick_2d_5_edge_skeleton.svg\
+  build/$(MODE)/svg/mesh/brick_2d_5_forman_edge_skeleton.svg\
   build/$(MODE)/svg/mesh/brick_2d_10_edge_skeleton.svg\
   build/$(MODE)/svg/mesh/brick_2d_10_forman_edge_skeleton.svg\
+  build/$(MODE)/svg/mesh/square_edge_skeleton.svg\
+  build/$(MODE)/svg/mesh/square_2_edge_skeleton.svg\
+  build/$(MODE)/svg/mesh/square_4_edge_skeleton.svg\
+  build/$(MODE)/svg/mesh/square_8_edge_skeleton.svg\
+  build/$(MODE)/svg/mesh/pentagon_edge_skeleton.svg\
+  build/$(MODE)/svg/mesh/pentagon_forman_edge_skeleton.svg\
+  build/$(MODE)/svg/mesh/two_triangles_edge_skeleton.svg\
+  build/$(MODE)/svg/mesh/two_triangles_forman_edge_skeleton.svg\
+  build/$(MODE)/svg/mesh/triangle_and_square_edge_skeleton.svg\
+  build/$(MODE)/svg/mesh/triangle_and_square_forman_edge_skeleton.svg\
+  build/$(MODE)/svg/mesh/2d_10_grains_edge_skeleton.svg\
+  build/$(MODE)/svg/mesh/2d_10_grains_forman_edge_skeleton.svg\
   build/$(MODE)/svg/mesh/circular_4_3_edge_skeleton.svg\
   build/$(MODE)/svg/mesh/circular_4_3_forman_edge_skeleton.svg\
   build/$(MODE)/svg/mesh/circular_18_10_edge_skeleton.svg\
   build/$(MODE)/svg/mesh/circular_18_10_forman_edge_skeleton.svg\
-  build/$(MODE)/svg/mesh/pentagon_edge_skeleton.svg\
-  build/$(MODE)/svg/mesh/pentagon_forman_edge_skeleton.svg\
-  build/$(MODE)/svg/mesh/2d_10_grains_edge_skeleton.svg\
-  build/$(MODE)/svg/mesh/2d_10_grains_forman_edge_skeleton.svg\
+
+build/$(MODE)/svg/mesh/brick_2d_1_edge_skeleton.svg:\
+  build/$(MODE)/bin/idec_graphics_mesh_2d_skeleton$(.EXE)\
+  build/$(MODE)/txt/mesh/brick_2d_1.txt\
+  build/$(MODE)/obj/plugins/idec_cairo_graphics_draw_functions_mesh_2d_skeleton$(.OBJ)\
+  build/$(MODE)/obj/plugins/idec_cairo_svg_animation$(.OBJ)\
+  | build/$(MODE)/svg/mesh\
+    build/$(MODE)/lib/plugins/libcanvas$(.DLL)\
+    build/$(MODE)/lib/plugins/libanimation$(.DLL)
+	$< \
+  --mesh=$(word 2, $^)\
+  --canvas-library=$(word 2, $|)\
+  --canvas-backend=idec_cairo_graphics_draw_functions_mesh_2d_skeleton\
+  --animation-library=$(word 3, $|)\
+  --animation-backend=idec_cairo_svg_animation\
+  --output=$@
+
+build/$(MODE)/svg/mesh/brick_2d_1_forman_edge_skeleton.svg:\
+  build/$(MODE)/bin/idec_graphics_mesh_2d_skeleton$(.EXE)\
+  build/$(MODE)/txt/mesh/brick_2d_1_forman.txt\
+  build/$(MODE)/obj/plugins/idec_cairo_graphics_draw_functions_mesh_2d_skeleton$(.OBJ)\
+  build/$(MODE)/obj/plugins/idec_cairo_svg_animation$(.OBJ)\
+  | build/$(MODE)/svg/mesh\
+    build/$(MODE)/lib/plugins/libcanvas$(.DLL)\
+    build/$(MODE)/lib/plugins/libanimation$(.DLL)
+	$< \
+  --mesh=$(word 2, $^)\
+  --canvas-library=$(word 2, $|)\
+  --canvas-backend=idec_cairo_graphics_draw_functions_mesh_2d_skeleton\
+  --animation-library=$(word 3, $|)\
+  --animation-backend=idec_cairo_svg_animation\
+  --output=$@
+
+build/$(MODE)/svg/mesh/brick_2d_2_edge_skeleton.svg:\
+  build/$(MODE)/bin/idec_graphics_mesh_2d_skeleton$(.EXE)\
+  build/$(MODE)/txt/mesh/brick_2d_2.txt\
+  build/$(MODE)/obj/plugins/idec_cairo_graphics_draw_functions_mesh_2d_skeleton$(.OBJ)\
+  build/$(MODE)/obj/plugins/idec_cairo_svg_animation$(.OBJ)\
+  | build/$(MODE)/svg/mesh\
+    build/$(MODE)/lib/plugins/libcanvas$(.DLL)\
+    build/$(MODE)/lib/plugins/libanimation$(.DLL)
+	$< \
+  --mesh=$(word 2, $^)\
+  --canvas-library=$(word 2, $|)\
+  --canvas-backend=idec_cairo_graphics_draw_functions_mesh_2d_skeleton\
+  --animation-library=$(word 3, $|)\
+  --animation-backend=idec_cairo_svg_animation\
+  --output=$@
+
+build/$(MODE)/svg/mesh/brick_2d_2_forman_edge_skeleton.svg:\
+  build/$(MODE)/bin/idec_graphics_mesh_2d_skeleton$(.EXE)\
+  build/$(MODE)/txt/mesh/brick_2d_2_forman.txt\
+  build/$(MODE)/obj/plugins/idec_cairo_graphics_draw_functions_mesh_2d_skeleton$(.OBJ)\
+  build/$(MODE)/obj/plugins/idec_cairo_svg_animation$(.OBJ)\
+  | build/$(MODE)/svg/mesh\
+    build/$(MODE)/lib/plugins/libcanvas$(.DLL)\
+    build/$(MODE)/lib/plugins/libanimation$(.DLL)
+	$< \
+  --mesh=$(word 2, $^)\
+  --canvas-library=$(word 2, $|)\
+  --canvas-backend=idec_cairo_graphics_draw_functions_mesh_2d_skeleton\
+  --animation-library=$(word 3, $|)\
+  --animation-backend=idec_cairo_svg_animation\
+  --output=$@
+
+build/$(MODE)/svg/mesh/brick_2d_4_edge_skeleton.svg:\
+  build/$(MODE)/bin/idec_graphics_mesh_2d_skeleton$(.EXE)\
+  build/$(MODE)/txt/mesh/brick_2d_4.txt\
+  build/$(MODE)/obj/plugins/idec_cairo_graphics_draw_functions_mesh_2d_skeleton$(.OBJ)\
+  build/$(MODE)/obj/plugins/idec_cairo_svg_animation$(.OBJ)\
+  | build/$(MODE)/svg/mesh\
+    build/$(MODE)/lib/plugins/libcanvas$(.DLL)\
+    build/$(MODE)/lib/plugins/libanimation$(.DLL)
+	$< \
+  --mesh=$(word 2, $^)\
+  --canvas-library=$(word 2, $|)\
+  --canvas-backend=idec_cairo_graphics_draw_functions_mesh_2d_skeleton\
+  --animation-library=$(word 3, $|)\
+  --animation-backend=idec_cairo_svg_animation\
+  --output=$@
+
+build/$(MODE)/svg/mesh/brick_2d_5_edge_skeleton.svg:\
+  build/$(MODE)/bin/idec_graphics_mesh_2d_skeleton$(.EXE)\
+  build/$(MODE)/txt/mesh/brick_2d_5.txt\
+  build/$(MODE)/obj/plugins/idec_cairo_graphics_draw_functions_mesh_2d_skeleton$(.OBJ)\
+  build/$(MODE)/obj/plugins/idec_cairo_svg_animation$(.OBJ)\
+  | build/$(MODE)/svg/mesh\
+    build/$(MODE)/lib/plugins/libcanvas$(.DLL)\
+    build/$(MODE)/lib/plugins/libanimation$(.DLL)
+	$< \
+  --mesh=$(word 2, $^)\
+  --canvas-library=$(word 2, $|)\
+  --canvas-backend=idec_cairo_graphics_draw_functions_mesh_2d_skeleton\
+  --animation-library=$(word 3, $|)\
+  --animation-backend=idec_cairo_svg_animation\
+  --output=$@
+
+build/$(MODE)/svg/mesh/brick_2d_5_forman_edge_skeleton.svg:\
+  build/$(MODE)/bin/idec_graphics_mesh_2d_skeleton$(.EXE)\
+  build/$(MODE)/txt/mesh/brick_2d_5_forman.txt\
+  build/$(MODE)/obj/plugins/idec_cairo_graphics_draw_functions_mesh_2d_skeleton$(.OBJ)\
+  build/$(MODE)/obj/plugins/idec_cairo_svg_animation$(.OBJ)\
+  | build/$(MODE)/svg/mesh\
+    build/$(MODE)/lib/plugins/libcanvas$(.DLL)\
+    build/$(MODE)/lib/plugins/libanimation$(.DLL)
+	$< \
+  --mesh=$(word 2, $^)\
+  --canvas-library=$(word 2, $|)\
+  --canvas-backend=idec_cairo_graphics_draw_functions_mesh_2d_skeleton\
+  --animation-library=$(word 3, $|)\
+  --animation-backend=idec_cairo_svg_animation\
+  --output=$@
 
 build/$(MODE)/svg/mesh/brick_2d_10_edge_skeleton.svg:\
   build/$(MODE)/bin/idec_graphics_mesh_2d_skeleton$(.EXE)\
@@ -34,6 +161,199 @@ build/$(MODE)/svg/mesh/brick_2d_10_edge_skeleton.svg:\
 build/$(MODE)/svg/mesh/brick_2d_10_forman_edge_skeleton.svg:\
   build/$(MODE)/bin/idec_graphics_mesh_2d_skeleton$(.EXE)\
   build/$(MODE)/txt/mesh/brick_2d_10_forman.txt\
+  build/$(MODE)/obj/plugins/idec_cairo_graphics_draw_functions_mesh_2d_skeleton$(.OBJ)\
+  build/$(MODE)/obj/plugins/idec_cairo_svg_animation$(.OBJ)\
+  | build/$(MODE)/svg/mesh\
+    build/$(MODE)/lib/plugins/libcanvas$(.DLL)\
+    build/$(MODE)/lib/plugins/libanimation$(.DLL)
+	$< \
+  --mesh=$(word 2, $^)\
+  --canvas-library=$(word 2, $|)\
+  --canvas-backend=idec_cairo_graphics_draw_functions_mesh_2d_skeleton\
+  --animation-library=$(word 3, $|)\
+  --animation-backend=idec_cairo_svg_animation\
+  --output=$@
+
+build/$(MODE)/svg/mesh/square_edge_skeleton.svg:\
+  build/$(MODE)/bin/idec_graphics_mesh_2d_skeleton$(.EXE)\
+  data/mesh/square.txt\
+  build/$(MODE)/obj/plugins/idec_cairo_graphics_draw_functions_mesh_2d_skeleton$(.OBJ)\
+  build/$(MODE)/obj/plugins/idec_cairo_svg_animation$(.OBJ)\
+  | build/$(MODE)/svg/mesh\
+    build/$(MODE)/lib/plugins/libcanvas$(.DLL)\
+    build/$(MODE)/lib/plugins/libanimation$(.DLL)
+	$< \
+  --mesh=$(word 2, $^)\
+  --canvas-library=$(word 2, $|)\
+  --canvas-backend=idec_cairo_graphics_draw_functions_mesh_2d_skeleton\
+  --animation-library=$(word 3, $|)\
+  --animation-backend=idec_cairo_svg_animation\
+  --output=$@
+
+build/$(MODE)/svg/mesh/square_2_edge_skeleton.svg:\
+  build/$(MODE)/bin/idec_graphics_mesh_2d_skeleton$(.EXE)\
+  build/$(MODE)/txt/mesh/square_2.txt\
+  build/$(MODE)/obj/plugins/idec_cairo_graphics_draw_functions_mesh_2d_skeleton$(.OBJ)\
+  build/$(MODE)/obj/plugins/idec_cairo_svg_animation$(.OBJ)\
+  | build/$(MODE)/svg/mesh\
+    build/$(MODE)/lib/plugins/libcanvas$(.DLL)\
+    build/$(MODE)/lib/plugins/libanimation$(.DLL)
+	$< \
+  --mesh=$(word 2, $^)\
+  --canvas-library=$(word 2, $|)\
+  --canvas-backend=idec_cairo_graphics_draw_functions_mesh_2d_skeleton\
+  --animation-library=$(word 3, $|)\
+  --animation-backend=idec_cairo_svg_animation\
+  --output=$@
+
+build/$(MODE)/svg/mesh/square_4_edge_skeleton.svg:\
+  build/$(MODE)/bin/idec_graphics_mesh_2d_skeleton$(.EXE)\
+  build/$(MODE)/txt/mesh/square_4.txt\
+  build/$(MODE)/obj/plugins/idec_cairo_graphics_draw_functions_mesh_2d_skeleton$(.OBJ)\
+  build/$(MODE)/obj/plugins/idec_cairo_svg_animation$(.OBJ)\
+  | build/$(MODE)/svg/mesh\
+    build/$(MODE)/lib/plugins/libcanvas$(.DLL)\
+    build/$(MODE)/lib/plugins/libanimation$(.DLL)
+	$< \
+  --mesh=$(word 2, $^)\
+  --canvas-library=$(word 2, $|)\
+  --canvas-backend=idec_cairo_graphics_draw_functions_mesh_2d_skeleton\
+  --animation-library=$(word 3, $|)\
+  --animation-backend=idec_cairo_svg_animation\
+  --output=$@
+
+build/$(MODE)/svg/mesh/square_8_edge_skeleton.svg:\
+  build/$(MODE)/bin/idec_graphics_mesh_2d_skeleton$(.EXE)\
+  build/$(MODE)/txt/mesh/square_8.txt\
+  build/$(MODE)/obj/plugins/idec_cairo_graphics_draw_functions_mesh_2d_skeleton$(.OBJ)\
+  build/$(MODE)/obj/plugins/idec_cairo_svg_animation$(.OBJ)\
+  | build/$(MODE)/svg/mesh\
+    build/$(MODE)/lib/plugins/libcanvas$(.DLL)\
+    build/$(MODE)/lib/plugins/libanimation$(.DLL)
+	$< \
+  --mesh=$(word 2, $^)\
+  --canvas-library=$(word 2, $|)\
+  --canvas-backend=idec_cairo_graphics_draw_functions_mesh_2d_skeleton\
+  --animation-library=$(word 3, $|)\
+  --animation-backend=idec_cairo_svg_animation\
+  --output=$@
+
+build/$(MODE)/svg/mesh/pentagon_edge_skeleton.svg:\
+  build/$(MODE)/bin/idec_graphics_mesh_2d_skeleton$(.EXE)\
+  data/mesh/pentagon.txt\
+  build/$(MODE)/obj/plugins/idec_cairo_graphics_draw_functions_mesh_2d_skeleton$(.OBJ)\
+  build/$(MODE)/obj/plugins/idec_cairo_svg_animation$(.OBJ)\
+  | build/$(MODE)/svg/mesh\
+    build/$(MODE)/lib/plugins/libcanvas$(.DLL)\
+    build/$(MODE)/lib/plugins/libanimation$(.DLL)
+	$< \
+  --mesh=$(word 2, $^)\
+  --canvas-library=$(word 2, $|)\
+  --canvas-backend=idec_cairo_graphics_draw_functions_mesh_2d_skeleton\
+  --animation-library=$(word 3, $|)\
+  --animation-backend=idec_cairo_svg_animation\
+  --output=$@
+
+build/$(MODE)/svg/mesh/pentagon_forman_edge_skeleton.svg:\
+  build/$(MODE)/bin/idec_graphics_mesh_2d_skeleton$(.EXE)\
+  build/$(MODE)/txt/mesh/pentagon_forman.txt\
+  build/$(MODE)/obj/plugins/idec_cairo_graphics_draw_functions_mesh_2d_skeleton$(.OBJ)\
+  build/$(MODE)/obj/plugins/idec_cairo_svg_animation$(.OBJ)\
+  | build/$(MODE)/svg/mesh\
+    build/$(MODE)/lib/plugins/libcanvas$(.DLL)\
+    build/$(MODE)/lib/plugins/libanimation$(.DLL)
+	$< \
+  --mesh=$(word 2, $^)\
+  --canvas-library=$(word 2, $|)\
+  --canvas-backend=idec_cairo_graphics_draw_functions_mesh_2d_skeleton\
+  --animation-library=$(word 3, $|)\
+  --animation-backend=idec_cairo_svg_animation\
+  --output=$@
+
+build/$(MODE)/svg/mesh/two_triangles_edge_skeleton.svg:\
+  build/$(MODE)/bin/idec_graphics_mesh_2d_skeleton$(.EXE)\
+  data/mesh/two_triangles.txt\
+  build/$(MODE)/obj/plugins/idec_cairo_graphics_draw_functions_mesh_2d_skeleton$(.OBJ)\
+  build/$(MODE)/obj/plugins/idec_cairo_svg_animation$(.OBJ)\
+  | build/$(MODE)/svg/mesh\
+    build/$(MODE)/lib/plugins/libcanvas$(.DLL)\
+    build/$(MODE)/lib/plugins/libanimation$(.DLL)
+	$< \
+  --mesh=$(word 2, $^)\
+  --canvas-library=$(word 2, $|)\
+  --canvas-backend=idec_cairo_graphics_draw_functions_mesh_2d_skeleton\
+  --animation-library=$(word 3, $|)\
+  --animation-backend=idec_cairo_svg_animation\
+  --output=$@
+
+build/$(MODE)/svg/mesh/two_triangles_forman_edge_skeleton.svg:\
+  build/$(MODE)/bin/idec_graphics_mesh_2d_skeleton$(.EXE)\
+  build/$(MODE)/txt/mesh/two_triangles_forman.txt\
+  build/$(MODE)/obj/plugins/idec_cairo_graphics_draw_functions_mesh_2d_skeleton$(.OBJ)\
+  build/$(MODE)/obj/plugins/idec_cairo_svg_animation$(.OBJ)\
+  | build/$(MODE)/svg/mesh\
+    build/$(MODE)/lib/plugins/libcanvas$(.DLL)\
+    build/$(MODE)/lib/plugins/libanimation$(.DLL)
+	$< \
+  --mesh=$(word 2, $^)\
+  --canvas-library=$(word 2, $|)\
+  --canvas-backend=idec_cairo_graphics_draw_functions_mesh_2d_skeleton\
+  --animation-library=$(word 3, $|)\
+  --animation-backend=idec_cairo_svg_animation\
+  --output=$@
+
+build/$(MODE)/svg/mesh/triangle_and_square_edge_skeleton.svg:\
+  build/$(MODE)/bin/idec_graphics_mesh_2d_skeleton$(.EXE)\
+  data/mesh/triangle_and_square.txt\
+  build/$(MODE)/obj/plugins/idec_cairo_graphics_draw_functions_mesh_2d_skeleton$(.OBJ)\
+  build/$(MODE)/obj/plugins/idec_cairo_svg_animation$(.OBJ)\
+  | build/$(MODE)/svg/mesh\
+    build/$(MODE)/lib/plugins/libcanvas$(.DLL)\
+    build/$(MODE)/lib/plugins/libanimation$(.DLL)
+	$< \
+  --mesh=$(word 2, $^)\
+  --canvas-library=$(word 2, $|)\
+  --canvas-backend=idec_cairo_graphics_draw_functions_mesh_2d_skeleton\
+  --animation-library=$(word 3, $|)\
+  --animation-backend=idec_cairo_svg_animation\
+  --output=$@
+
+build/$(MODE)/svg/mesh/triangle_and_square_forman_edge_skeleton.svg:\
+  build/$(MODE)/bin/idec_graphics_mesh_2d_skeleton$(.EXE)\
+  build/$(MODE)/txt/mesh/triangle_and_square_forman.txt\
+  build/$(MODE)/obj/plugins/idec_cairo_graphics_draw_functions_mesh_2d_skeleton$(.OBJ)\
+  build/$(MODE)/obj/plugins/idec_cairo_svg_animation$(.OBJ)\
+  | build/$(MODE)/svg/mesh\
+    build/$(MODE)/lib/plugins/libcanvas$(.DLL)\
+    build/$(MODE)/lib/plugins/libanimation$(.DLL)
+	$< \
+  --mesh=$(word 2, $^)\
+  --canvas-library=$(word 2, $|)\
+  --canvas-backend=idec_cairo_graphics_draw_functions_mesh_2d_skeleton\
+  --animation-library=$(word 3, $|)\
+  --animation-backend=idec_cairo_svg_animation\
+  --output=$@
+
+build/$(MODE)/svg/mesh/2d_10_grains_edge_skeleton.svg:\
+  build/$(MODE)/bin/idec_graphics_mesh_2d_skeleton$(.EXE)\
+  data/mesh/2d_10_grains.tess\
+  build/$(MODE)/obj/plugins/idec_cairo_graphics_draw_functions_mesh_2d_skeleton$(.OBJ)\
+  build/$(MODE)/obj/plugins/idec_cairo_svg_animation$(.OBJ)\
+  | build/$(MODE)/svg/mesh\
+    build/$(MODE)/lib/plugins/libcanvas$(.DLL)\
+    build/$(MODE)/lib/plugins/libanimation$(.DLL)
+	$< \
+  --mesh-format=tess\
+  --mesh=$(word 2, $^)\
+  --canvas-library=$(word 2, $|)\
+  --canvas-backend=idec_cairo_graphics_draw_functions_mesh_2d_skeleton\
+  --animation-library=$(word 3, $|)\
+  --animation-backend=idec_cairo_svg_animation\
+  --output=$@
+
+build/$(MODE)/svg/mesh/2d_10_grains_forman_edge_skeleton.svg:\
+  build/$(MODE)/bin/idec_graphics_mesh_2d_skeleton$(.EXE)\
+  build/$(MODE)/txt/mesh/2d_10_grains_forman.txt\
   build/$(MODE)/obj/plugins/idec_cairo_graphics_draw_functions_mesh_2d_skeleton$(.OBJ)\
   build/$(MODE)/obj/plugins/idec_cairo_svg_animation$(.OBJ)\
   | build/$(MODE)/svg/mesh\
@@ -98,71 +418,6 @@ build/$(MODE)/svg/mesh/circular_18_10_edge_skeleton.svg:\
 build/$(MODE)/svg/mesh/circular_18_10_forman_edge_skeleton.svg:\
   build/$(MODE)/bin/idec_graphics_mesh_2d_skeleton$(.EXE)\
   build/$(MODE)/txt/mesh/circular_18_10_forman.txt\
-  build/$(MODE)/obj/plugins/idec_cairo_graphics_draw_functions_mesh_2d_skeleton$(.OBJ)\
-  build/$(MODE)/obj/plugins/idec_cairo_svg_animation$(.OBJ)\
-  | build/$(MODE)/svg/mesh\
-    build/$(MODE)/lib/plugins/libcanvas$(.DLL)\
-    build/$(MODE)/lib/plugins/libanimation$(.DLL)
-	$< \
-  --mesh=$(word 2, $^)\
-  --canvas-library=$(word 2, $|)\
-  --canvas-backend=idec_cairo_graphics_draw_functions_mesh_2d_skeleton\
-  --animation-library=$(word 3, $|)\
-  --animation-backend=idec_cairo_svg_animation\
-  --output=$@
-
-build/$(MODE)/svg/mesh/pentagon_edge_skeleton.svg:\
-  build/$(MODE)/bin/idec_graphics_mesh_2d_skeleton$(.EXE)\
-  data/mesh/pentagon.txt\
-  build/$(MODE)/obj/plugins/idec_cairo_graphics_draw_functions_mesh_2d_skeleton$(.OBJ)\
-  build/$(MODE)/obj/plugins/idec_cairo_svg_animation$(.OBJ)\
-  | build/$(MODE)/svg/mesh\
-    build/$(MODE)/lib/plugins/libcanvas$(.DLL)\
-    build/$(MODE)/lib/plugins/libanimation$(.DLL)
-	$< \
-  --mesh=$(word 2, $^)\
-  --canvas-library=$(word 2, $|)\
-  --canvas-backend=idec_cairo_graphics_draw_functions_mesh_2d_skeleton\
-  --animation-library=$(word 3, $|)\
-  --animation-backend=idec_cairo_svg_animation\
-  --output=$@
-
-build/$(MODE)/svg/mesh/pentagon_forman_edge_skeleton.svg:\
-  build/$(MODE)/bin/idec_graphics_mesh_2d_skeleton$(.EXE)\
-  build/$(MODE)/txt/mesh/pentagon_forman.txt\
-  build/$(MODE)/obj/plugins/idec_cairo_graphics_draw_functions_mesh_2d_skeleton$(.OBJ)\
-  build/$(MODE)/obj/plugins/idec_cairo_svg_animation$(.OBJ)\
-  | build/$(MODE)/svg/mesh\
-    build/$(MODE)/lib/plugins/libcanvas$(.DLL)\
-    build/$(MODE)/lib/plugins/libanimation$(.DLL)
-	$< \
-  --mesh=$(word 2, $^)\
-  --canvas-library=$(word 2, $|)\
-  --canvas-backend=idec_cairo_graphics_draw_functions_mesh_2d_skeleton\
-  --animation-library=$(word 3, $|)\
-  --animation-backend=idec_cairo_svg_animation\
-  --output=$@
-
-build/$(MODE)/svg/mesh/2d_10_grains_edge_skeleton.svg:\
-  build/$(MODE)/bin/idec_graphics_mesh_2d_skeleton$(.EXE)\
-  data/mesh/2d_10_grains.tess\
-  build/$(MODE)/obj/plugins/idec_cairo_graphics_draw_functions_mesh_2d_skeleton$(.OBJ)\
-  build/$(MODE)/obj/plugins/idec_cairo_svg_animation$(.OBJ)\
-  | build/$(MODE)/svg/mesh\
-    build/$(MODE)/lib/plugins/libcanvas$(.DLL)\
-    build/$(MODE)/lib/plugins/libanimation$(.DLL)
-	$< \
-  --mesh-format=tess\
-  --mesh=$(word 2, $^)\
-  --canvas-library=$(word 2, $|)\
-  --canvas-backend=idec_cairo_graphics_draw_functions_mesh_2d_skeleton\
-  --animation-library=$(word 3, $|)\
-  --animation-backend=idec_cairo_svg_animation\
-  --output=$@
-
-build/$(MODE)/svg/mesh/2d_10_grains_forman_edge_skeleton.svg:\
-  build/$(MODE)/bin/idec_graphics_mesh_2d_skeleton$(.EXE)\
-  build/$(MODE)/txt/mesh/2d_10_grains_forman.txt\
   build/$(MODE)/obj/plugins/idec_cairo_graphics_draw_functions_mesh_2d_skeleton$(.OBJ)\
   build/$(MODE)/obj/plugins/idec_cairo_svg_animation$(.OBJ)\
   | build/$(MODE)/svg/mesh\
