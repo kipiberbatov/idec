0434251a94e2339b8a342819c91f7c7747ae7470
Kiprian Berbatov <kiprian.berbatov@manchester.ac.uk>
2025-03-26 16:41:50 +0000

    Abstract drawing of flow directions in 1-cochains

--- code/c/include/graphics/idec_graphics_mesh_2d_1_cochain_sequence_draw_functions.h
+++ code/c/include/graphics/idec_graphics_mesh_2d_1_cochain_sequence_draw_functions.h
@@ -1,12 +1,22 @@
 #ifndef _idec_graphics_mesh_2d_1_cochain_sequence_draw_functions_h
 #define _idec_graphics_mesh_2d_1_cochain_sequence_draw_functions_h
 
+struct idec_graphics_mesh_2d_1_cochain_sequence;
 struct idec_graphics_mesh_2d_edge;
 struct idec_graphics_mesh_2d_skeleton;
 
 struct idec_graphics_mesh_2d_1_cochain_sequence_draw_functions
 {
   void (*set_background_color)(void *, int *);
+  void (*draw_values)(
+    void *,
+    int *,
+    const struct idec_graphics_mesh_2d_1_cochain_sequence *,
+    int,
+    int,
+    void (*)(void *, int *, int, int),
+    void (**)(void *, int *, const void *),
+    void (*)(void *, int *, const struct idec_graphics_mesh_2d_edge *));
   void (*set_color)(void *, int *, int, int);
   void (*draw_oriented_edge)(
     void *, int *, const struct idec_graphics_mesh_2d_edge *);
