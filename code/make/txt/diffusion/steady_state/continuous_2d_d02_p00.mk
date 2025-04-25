f5578cf3d6ac00e5cd538d79d4928132b41d97af
Kiprian Berbatov <kiprian.berbatov@manchester.ac.uk>
2025-04-25 06:21:59 +0000

    Calculate differences and relative errors for 2d_d02_p00

--- code/make/txt/diffusion/steady_state/continuous_2d_d02_p00.mk
+++ code/make/txt/diffusion/steady_state/continuous_2d_d02_p00.mk
@@ -9,15 +9,29 @@ build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00:\
 _txt_diffusion_steady_state_continuous_2d_d02_p00 :=\
   build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/exact_2d_10_grains_forman_potential.txt\
   build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/exact_2d_10_grains_forman_flow_rate.txt\
+  build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/exact_2d_10_grains_forman_potential_norm_2.txt\
+  build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/exact_2d_10_grains_forman_flow_rate_norm_2.txt\
   build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/primal_strong_cochain_2d_10_grains_forman_input.txt\
   build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/primal_strong_cochain_2d_10_grains_forman_potential.txt\
   build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/primal_strong_cochain_2d_10_grains_forman_flow_rate.txt\
   build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/primal_weak_cochain_2d_10_grains_forman_input.txt\
   build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/primal_weak_cochain_2d_10_grains_forman_potential.txt\
   build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/primal_weak_cochain_2d_10_grains_forman_flow_rate.txt\
+  build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/primal_weak_cochain_2d_10_grains_forman_potential_difference_with_exact.txt\
+  build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/primal_weak_cochain_2d_10_grains_forman_flow_rate_difference_with_exact.txt\
+  build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/primal_weak_cochain_2d_10_grains_forman_potential_difference_with_exact_norm_2.txt\
+  build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/primal_weak_cochain_2d_10_grains_forman_flow_rate_difference_with_exact_norm_2.txt\
+  build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/primal_weak_cochain_2d_10_grains_forman_potential_relative_error_with_exact_norm_2.txt\
+  build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/primal_weak_cochain_2d_10_grains_forman_flow_rate_relative_error_with_exact_norm_2.txt\
   build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/mixed_weak_cochain_2d_10_grains_forman_input.txt\
   build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/mixed_weak_cochain_2d_10_grains_forman_solution.txt\
   build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/mixed_weak_cochain_2d_10_grains_forman_potential.txt\
+  build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/mixed_weak_cochain_2d_10_grains_forman_potential_difference_with_exact.txt\
+  build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/mixed_weak_cochain_2d_10_grains_forman_flow_rate_difference_with_exact.txt\
+  build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/mixed_weak_cochain_2d_10_grains_forman_potential_difference_with_exact_norm_2.txt\
+  build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/mixed_weak_cochain_2d_10_grains_forman_flow_rate_difference_with_exact_norm_2.txt\
+  build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/mixed_weak_cochain_2d_10_grains_forman_potential_relative_error_with_exact_norm_2.txt\
+  build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/mixed_weak_cochain_2d_10_grains_forman_flow_rate_relative_error_with_exact_norm_2.txt\
 
 build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/exact_2d_10_grains_forman_potential.txt:\
   build/$(MODE)/bin/diffusion_steady_state_continuous_exact_potential$(.EXE)\
@@ -42,6 +56,24 @@ build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/exact_2d_10_grain
   $(word 2, $|)\
   diffusion_steady_state_continuous_2d_d02_p00_flow_rate\
   --raw > $@
+
+build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/exact_2d_10_grains_forman_potential_norm_2.txt:\
+  build/$(MODE)/bin/double_array_lebesgue_norm$(.EXE)\
+  build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/exact_2d_10_grains_forman_potential.txt\
+  | build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00
+	$<\
+  --size=63\
+  --vector=$(word 2, $^)\
+  --output=$@
+
+build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/exact_2d_10_grains_forman_flow_rate_norm_2.txt:\
+  build/$(MODE)/bin/double_array_lebesgue_norm$(.EXE)\
+  build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/exact_2d_10_grains_forman_flow_rate.txt\
+  | build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00
+	$<\
+  --size=111\
+  --vector=$(word 2, $^)\
+  --output=$@
 
 build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/primal_strong_cochain_2d_10_grains_forman_input.txt:\
   build/$(MODE)/bin/diffusion_steady_state_discrete_primal_strong_from_continuous$(.EXE)\
@@ -123,6 +155,66 @@ build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/primal_weak_cocha
   --raw $(word 5, $^)\
   --raw > $@
 
+build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/primal_weak_cochain_2d_10_grains_forman_potential_difference_with_exact.txt:\
+  build/$(MODE)/bin/double_array_subtract$(.EXE)\
+  build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/primal_weak_cochain_2d_10_grains_forman_potential.txt\
+  build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/exact_2d_10_grains_forman_potential.txt\
+  | build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00
+	$<\
+  --size=63\
+  --minuend=$(word 2, $^)\
+  --subtrahend=$(word 3, $^)\
+  --output=$@
+
+build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/primal_weak_cochain_2d_10_grains_forman_flow_rate_difference_with_exact.txt:\
+  build/$(MODE)/bin/double_array_subtract$(.EXE)\
+  build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/primal_weak_cochain_2d_10_grains_forman_flow_rate.txt\
+  build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/exact_2d_10_grains_forman_flow_rate.txt\
+  | build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00
+	$<\
+  --size=111\
+  --minuend=$(word 2, $^)\
+  --subtrahend=$(word 3, $^)\
+  --output=$@
+
+build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/primal_weak_cochain_2d_10_grains_forman_potential_difference_with_exact_norm_2.txt:\
+  build/$(MODE)/bin/double_array_lebesgue_norm$(.EXE)\
+  build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/primal_weak_cochain_2d_10_grains_forman_potential_difference_with_exact.txt\
+  | build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00
+	$<\
+  --size=63\
+  --vector=$(word 2, $^)\
+  --output=$@
+
+build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/primal_weak_cochain_2d_10_grains_forman_flow_rate_difference_with_exact_norm_2.txt:\
+  build/$(MODE)/bin/double_array_lebesgue_norm$(.EXE)\
+  build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/primal_weak_cochain_2d_10_grains_forman_flow_rate_difference_with_exact.txt\
+  | build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00
+	$<\
+  --size=111\
+  --vector=$(word 2, $^)\
+  --output=$@
+
+build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/primal_weak_cochain_2d_10_grains_forman_potential_relative_error_with_exact_norm_2.txt:\
+  build/$(MODE)/bin/double_divide$(.EXE)\
+  build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/primal_weak_cochain_2d_10_grains_forman_potential_difference_with_exact_norm_2.txt\
+  build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/exact_2d_10_grains_forman_potential_norm_2.txt\
+  | build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00
+	$<\
+  --numerator=$(word 2, $^)\
+  --denominator=$(word 3, $^)\
+  --output=$@
+
+build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/primal_weak_cochain_2d_10_grains_forman_flow_rate_relative_error_with_exact_norm_2.txt:\
+  build/$(MODE)/bin/double_divide$(.EXE)\
+  build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/primal_weak_cochain_2d_10_grains_forman_flow_rate_difference_with_exact_norm_2.txt\
+  build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/exact_2d_10_grains_forman_flow_rate_norm_2.txt\
+  | build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00
+	$<\
+  --numerator=$(word 2, $^)\
+  --denominator=$(word 3, $^)\
+  --output=$@
+
 build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/mixed_weak_cochain_2d_10_grains_forman_input.txt:\
   build/$(MODE)/bin/diffusion_steady_state_discrete_mixed_weak_from_continuous$(.EXE)\
   build/$(MODE)/txt/mesh/2d_10_grains_forman.txt\
@@ -162,6 +254,67 @@ build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/mixed_weak_cochai
   $(word 4, $^)\
   $(word 5, $^)\
   > $@
+
+build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/mixed_weak_cochain_2d_10_grains_forman_potential_difference_with_exact.txt:\
+  build/$(MODE)/bin/double_array_subtract$(.EXE)\
+  build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/mixed_weak_cochain_2d_10_grains_forman_potential.txt\
+  build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/exact_2d_10_grains_forman_potential.txt\
+  | build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00
+	$<\
+  --size=63\
+  --minuend=$(word 2, $^)\
+  --subtrahend=$(word 3, $^)\
+  --output=$@
+
+build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/mixed_weak_cochain_2d_10_grains_forman_flow_rate_difference_with_exact.txt:\
+  build/$(MODE)/bin/double_array_subtract$(.EXE)\
+  build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/mixed_weak_cochain_2d_10_grains_forman_solution.txt\
+  build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/exact_2d_10_grains_forman_flow_rate.txt\
+  | build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00
+	$<\
+  --size=111\
+  --minuend-format=--steady-state-mixed-weak-solution-flow-rate-raw\
+  --minuend=$(word 2, $^)\
+  --subtrahend=$(word 3, $^)\
+  --output=$@
+
+build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/mixed_weak_cochain_2d_10_grains_forman_potential_difference_with_exact_norm_2.txt:\
+  build/$(MODE)/bin/double_array_lebesgue_norm$(.EXE)\
+  build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/mixed_weak_cochain_2d_10_grains_forman_potential_difference_with_exact.txt\
+  | build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00
+	$<\
+  --size=63\
+  --vector=$(word 2, $^)\
+  --output=$@
+
+build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/mixed_weak_cochain_2d_10_grains_forman_flow_rate_difference_with_exact_norm_2.txt:\
+  build/$(MODE)/bin/double_array_lebesgue_norm$(.EXE)\
+  build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/mixed_weak_cochain_2d_10_grains_forman_flow_rate_difference_with_exact.txt\
+  | build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00
+	$<\
+  --size=111\
+  --vector=$(word 2, $^)\
+  --output=$@
+
+build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/mixed_weak_cochain_2d_10_grains_forman_potential_relative_error_with_exact_norm_2.txt:\
+  build/$(MODE)/bin/double_divide$(.EXE)\
+  build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/mixed_weak_cochain_2d_10_grains_forman_potential_difference_with_exact_norm_2.txt\
+  build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/exact_2d_10_grains_forman_potential_norm_2.txt\
+  | build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00
+	$<\
+  --numerator=$(word 2, $^)\
+  --denominator=$(word 3, $^)\
+  --output=$@
+
+build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/mixed_weak_cochain_2d_10_grains_forman_flow_rate_relative_error_with_exact_norm_2.txt:\
+  build/$(MODE)/bin/double_divide$(.EXE)\
+  build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/mixed_weak_cochain_2d_10_grains_forman_flow_rate_difference_with_exact_norm_2.txt\
+  build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00/exact_2d_10_grains_forman_flow_rate_norm_2.txt\
+  | build/$(MODE)/txt/diffusion/steady_state/continuous_2d_d02_p00
+	$<\
+  --numerator=$(word 2, $^)\
+  --denominator=$(word 3, $^)\
+  --output=$@
 
 txt_diffusion_steady_state_continuous_2d_d02_p00:\
   $(_txt_diffusion_steady_state_continuous_2d_d02_p00)
